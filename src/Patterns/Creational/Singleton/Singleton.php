654facc5228afe2b01f838b65e8cafe8c513f80e
viktorov <i.viktorov@raketa.travel>
2022-02-12 17:30:23 +0500

    Singleton

---
+++ src/Patterns/Creational/Singleton/Singleton.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Patterns\Creational\Singleton;
+
+class Singleton
+{
+    private static self $instance;
+
+    protected function __construct()
+    {
+
+    }
+
+    public function getInstance(): self
+    {
+        if (!isset(static::$instance)) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+}
